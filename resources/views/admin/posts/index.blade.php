0564e62c0369b5ad5e560c05ea4b0438407fb4c4
Claudio <claudiodicaprio89@gmail.com>
2022-02-15 16:19:15 +0100

    bah

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <a href="{{route('admin.posts/show')}}"><button type="button" class="btn btn-primary">Primary</button></a> --}}
+{{-- <a href="{{route("posts.show",$post->id)}}"><button type="button" class="btn btn-primary">Sfoglia</button></a> --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
