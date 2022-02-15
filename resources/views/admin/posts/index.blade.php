d6249ecec790671c62c378de3fb88c0066cdc882
Claudio <claudiodicaprio89@gmail.com>
2022-02-15 16:11:58 +0100

    show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- <a href="{{route('admin.posts/show')}}"><button type="button" class="btn btn-primary">Primary</button></a> --}}
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
